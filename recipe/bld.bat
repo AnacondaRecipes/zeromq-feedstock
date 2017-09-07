0ea5f9dc21dba352201f44918fc7dceac2a7e656
Ray Donnelly <mingw.android@gmail.com>
2017-09-07 13:41:24 +0100

    Experimenting with latest git to see if it crashes (it does)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,12 @@
 mkdir build
 cd build
-cmake -G "NMake Makefiles" -D ENABLE_DRAFTS=OFF -D WITH_PERF_TOOL=OFF -D ZMQ_BUILD_TESTS=ON -D ENABLE_CPACK=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+cmake -G "NMake Makefiles" -D ENABLE_DRAFTS=OFF -D WITH_PERF_TOOL=OFF -D ZMQ_BUILD_TESTS=ON -D ENABLE_CPACK=OFF -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
 if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
-script:
-copy /y %LIBRARY_BIN%\libzmq-mt-4*.dll %LIBRARY_BIN%\libzmq.dll
+copy /y %LIBRARY_BIN%\libzmq-mt-*.dll %LIBRARY_BIN%\libzmq.dll
 if errorlevel 1 exit 1
-copy /y %LIBRARY_LIB%\libzmq-mt-4*.lib %LIBRARY_BIN%\libzmq.lib
+copy /y %LIBRARY_LIB%\libzmq-mt-*.lib %LIBRARY_BIN%\libzmq.lib
 if errorlevel 1 exit 1
 
 .\bin\test_ancillaries
